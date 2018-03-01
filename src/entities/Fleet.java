3b055a618d31f2596a269a24c07726149bad2211
luis40895 <luis40895@gmail.com>
2018-03-01 19:14:20 +0100

    classes

--- src/entities/Fleet.java
+++ src/entities/Fleet.java
@@ -1,5 +1,28 @@
 package entities;
 
+import java.util.List;
+
 public class Fleet {
 
+	private List<Car> carList;
+	
+	
+	public Fleet() {
+		super();
+	}
+	
+	public Fleet(List<Car> carList) {
+		super();
+		this.carList = carList;
+	}
+
+
+	public List<Car> getCarList() {
+		return carList;
+	}
+
+	public void setCarList(List<Car> carList) {
+		this.carList = carList;
+	}
+	
 }
